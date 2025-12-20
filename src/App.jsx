6339f05410117a6cc338b1bb654fb61e6ef0b507
Your Name <your.email@example.com>
2025-12-20 16:25:42 +0530

    why hope3 page added and hope3 journey added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import WhyHope3 from './pages/whyHope3.jsx'
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import Home from './pages/Home.jsx'
+// import WhyHope3 from './pages/WhyHope3.jsx'
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/why-hope3" element={<WhyHope3 />} />
+//       </Routes>
+//     </Router>
+//   )
+// }
+
+// export default App
+
+
+// src/App.jsx
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import WhyHope3 from "./pages/WhyHope3.jsx";
+import Hope3Journey from "./pages/Hope3Journey.jsx";
 
 function App() {
   return (
-    <Router>
+    <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/why-hope3" element={<WhyHope3 />} />
+        <Route path="/hope3-journey" element={<Hope3Journey />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
